1aaaf82aad4218d5f72f68d6d101427a4ba29624
Hydra0xetc <lordxzaodeo@gmail.com>
2025-08-26 10:40:43 +0700

    blablabla

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,29 +16,30 @@ return {
           },
         },
 
-        -- lua_ls = {
-        --   cmd = { "/data/data/com.termux/files/usr/bin/lua-language-server" },
-        --   settings = {
-        --     Lua = {
-        --       runtime = {
-        --         version = 'LuaJIT',
-        --         path = vim.split(package.path, ';'),
-        --       },
-        --       diagnostics = {
-        --         globals = {'vim'},
-        --       },
-        --       workspace = {
-        --         library = vim.api.nvim_get_runtime_file("", true),
-        --       },
-        --       telemetry = {
-        --         enable = false,
-        --       },
-        --     },
-        --   },
-        -- },
+        lua_ls = {
+          cmd = { "/data/data/com.termux/files/usr/bin/lua-language-server" },
+          settings = {
+            Lua = {
+              runtime = {
+                version = 'LuaJIT',
+                path = vim.split(package.path, ';'),
+              },
+              diagnostics = {
+                globals = {'vim'},
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = {
+                enable = false,
+              },
+            },
+          },
+        },
         bashls = {},
         jsonls = {},
         html = {},
+        ts_ls = {},
 
         -- Nonaktifkan pyright
         pyright = false,
@@ -64,42 +65,3 @@ return {
     },
   },
 }
--- vim.lsp.handlers["textDocument/inlayHint"] = function() end
---
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         clangd = {
---           mason = false,
---           cmd = {
---             "clangd",
---             "--compile-commands-dir=.",
---           },
---           init_options = {
---             compilationDatabasePath = ".",
---           },
---         },
---         lua_ls = {},
---         bashls = {},
---
---         -- basedpyright = {
---         --   cmd = { "basedpyright" },
---         --   settings = {
---         --     python {
---         --       analysis = {
---         --         autoSearchPath = true,
---         --         useLibraryCodeForTypes = true,
---         --         diagnosticMode = "workspace",
---         --         typeCheckingMode = "off",
---         --       },
---         --     },
---         --   },
---         -- },
---         -- pyright = {},
---         gopls = {},
---       },
---     },
---   },
--- }
