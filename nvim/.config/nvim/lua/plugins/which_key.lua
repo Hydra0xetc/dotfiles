58563152a6511179d4d3487e13c395c6794609e9
Hydra0xetc <lordxzaodeo@gmail.com>
2025-09-05 23:44:10 +0700

    add which key config

---
+++ nvim/.config/nvim/lua/plugins/which_key.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "classic", -- lebih minimal
+      plugins = { spelling = false }, -- matikan tambahan
+      triggers = {}, -- matikan trigger (jadi nggak muncul otomatis)
+    },
+  },
+}
