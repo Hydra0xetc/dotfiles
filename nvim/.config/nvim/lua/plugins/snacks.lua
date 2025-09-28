c26ac14d11fd4d5a433f75054ccba8d83a55c9f6
Hydra0xetc <lordxzaodeo@gmail.com>
2025-09-28 23:05:15 +0700

    add some customize snack dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,83 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
+    opts.dashboard = {
+      enabled = true,
+      hide_on_blank = false,
+      preset = {
+        pick = function(cmd)
+          return LazyVim.pick(cmd)()
+        end,
+
+        header = [[
+⡇⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢁⣿⣿⡏⠉⠉⠉⠉⠉⠉⠉⠉⠉⢹⣇⠉⠉⢳⣌⢉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀⢻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣰⠀⠀⢸⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣄⠀⠹⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⢸⢸⣿⡿⠿⢃⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⣿⣦⣀⠙⢿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⣛⣋⡄⢨⣨⣶⣶⣶⣷⡶⡀⠀⠀⠀⠀⠀⢀⠀⢿⣷⣶⣶⣶⣶⣷⣤⣭⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡄⣇⣟⠿⣿⣿⣷⣱⡀⠀⠀⠀⠀⢸⡄⢸⣿⠿⢛⠛⠛⠻⢿⡟⣽⠆⡀⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠂⠀⠀⠓⠀⠐⠒⠈⠻⣷⣌⠟⣿⣧⣵⡐⡄⠀⠀⠸⡿⠀⠀⠀⢀⡀⠀⠀⠀⠈⠃⢜⡁⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⠀⠀⠠⠤⠀⠀⠈⠻⣦⣽⣿⣿⣷⡜⠆⠀⠀⣵⡆⠀⠈⠛⠁⠀⠀⢢⡀⠘⢿⡇⠀⠀⠀⠀⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⢀⢡⡀⢀⢠⠀⠀⠆⡀⣿⣧⣼⣿⣿⣿⣿⣯⣼⣷⣄⠸⣇⢠⣲⣤⣜⣠⢀⣾⠟⣠⣾⣇⠀⠀⣠⣤⠀⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠈⠆⣩⣈⠻⣿⣿⠞⣡⡿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣜⡢⢝⣻⠛⠋⠵⠖⠖⣢⣿⡇⠀⠀⣭⡹⣇⠀⠀⡇
+⡇⠀⠀⠀⠀⠀⠀⠀⠀⠰⣶⣭⣛⣓⣂⣭⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡖⢶⠒⠿⠷⢿⣿⣿⡇⠀⠈⣿⢡⣿ ⠀⡇
+⡇ ⠀⠀⠀⠀⠀⠀⠀⠀⠉⣋⠉⣫⣼⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣶⣶⣾⣿⣿⠀⢀⡠⢟⣼⠏ ⠀⡇
+⡇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢀⣼⡰⢛⣁⣀⣀⣀⡇
+]],
+        keys = {
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = function()
+              require("snacks.dashboard").pick("files")
+            end
+          },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = function()
+              require("snacks.dashboard").pick("live_grep")
+            end
+          },
+          -- {
+          --   icon = " ",
+          --   key = "r",
+          --   desc = "Recent Files",
+          --   action = function()
+          --     require("snacks.dashboard").pick("oldfiles")
+          --   end
+          -- },
+          -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          {
+            icon = " ",
+            key = "x",
+            desc = "Lazy Extras",
+            action = function()
+              vim.cmd("LazyExtras")
+            end
+          },
+          {
+            icon = "󰒲 ",
+            key = "l",
+            desc = "Lazy",
+            action = function()
+              vim.cmd("Lazy")
+            end
+          },
+          {
+            icon = " ",
+            key = "q",
+            desc = "Quit",
+            action = function()
+              vim.cmd("qa")
+            end
+          },
+        },
+      },
+    }
+
     opts = opts or {}
     opts.picker = opts.picker or {}
     opts.picker.files = opts.picker.files or {}
@@ -11,7 +88,6 @@ return {
         return item
       end
 
-      -- bangun path absolut item (cwd + file)
       local cwd = item.cwd
       local item_path = vim.fn.fnamemodify(cwd .. "/" .. item.file, ":p")
       local current = vim.fn.expand("%:p")
