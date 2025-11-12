a0f0daae2360309dbfd17a8361a4acf4136246de
Hydra0xetc <lordxzaodeo@gmail.com>
2025-11-13 01:50:04 +0700

    delete lazyvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set('n', 'r', '<C-r>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = {
-    style = "moon", -- "storm", "night", "day", atau "moon"
-    transparent = true,
-  },
-}
